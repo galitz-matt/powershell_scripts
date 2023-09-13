7f25bd96e5a40f76736d974ea916d3283b5f6bb4
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-09-12 20:46:30 -0400

    fix: removed '>>' before each line in rename_lowercase.ps1

--- rename_lowercase.ps1
+++ rename_lowercase.ps1
@@ -1,11 +1,11 @@
 
 #folderPath : relative path of working directory
 $files = Get-ChildItem $folderPath
-foreach($file in $files){
->> $currentName = $file.Name
->> $newName = $currentName.ToLower()
->> $tempName = "_temp_" + $curretName
->> $tempPath = Join-Path -Path $folderPath -ChildPath $tempName
->> Rename-Item -Path $file.FullName -NewName $tempName
->> Rename-Item -Path $tempPath -NewName $newName
->> }
+foreach ($file in $files) {
+    $currentName = $file.Name
+    $newName = $currentName.ToLower()
+    $tempName = "_temp_" + $curretName
+    $tempPath = Join-Path -Path $folderPath -ChildPath $tempName
+    Rename-Item -Path $file.FullName -NewName $tempName
+    Rename-Item -Path $tempPath -NewName $newName
+}
