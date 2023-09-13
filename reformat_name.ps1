f582f930c8be47576520f1f7bf3b0005aa6e7c70
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-09-12 22:55:59 -0400

    feat: script will replace ' ' in file name with '_'

--- reformat_name.ps1
+++ reformat_name.ps1
@@ -2,9 +2,13 @@
 $folderPath = Read-Host "Enter relative path of directory"
 $files = Get-ChildItem $folderPath
 foreach ($file in $files) {
-    $currentName = $file.Name
-    $newName = $currentName.ToLower()
-    $tempName = "_temp_" + $curretName
+    $workingName = $file.Name
+    if ($workingName -like "* *") {
+        $nameParts = $workingName -split " "
+        $workingName = $nameParts -join "_"
+    }
+    $newName = $workingName.ToLower()
+    $tempName = "_temp_" + $workingName
     $tempPath = Join-Path -Path $folderPath -ChildPath $tempName
     Rename-Item -Path $file.FullName -NewName $tempName
     Rename-Item -Path $tempPath -NewName $newName
